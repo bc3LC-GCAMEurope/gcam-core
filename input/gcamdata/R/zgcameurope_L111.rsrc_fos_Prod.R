cf950a367483f3f8948193328497fe20e3f2bc26
russellhz <horowitz.russell@gmail.com>
2024-04-11 11:56:05 +0200

    redistribute fossil fuel production

--- input/gcamdata/R/zgcameurope_L111.rsrc_fos_Prod.R
+++ input/gcamdata/R/zgcameurope_L111.rsrc_fos_Prod.R
@@ -17,8 +17,17 @@
 #' @importFrom tidyr complete replace_na
 #' @author RH January 2024
 module_gcameurope_L111.rsrc_fos_Prod <- function(command, ...) {
+  MODULE_INPUTS <- c(FILE = "common/GCAM32_to_EU",
+                     FILE = "energy/mappings/IEA_product_rsrc",
+                     FILE = "energy/rsrc_unconv_oil_prod_bbld",
+                     FILE = "energy/A11.fos_curves",
+                     "L100.IEA_en_bal_ctry_hist",
+                     "L1012.en_bal_EJ_R_Si_Fi_Yh_EUR",
+                     "L100.IEA_en_bal_ctry_hist",
+                     "L1012.en_bal_EJ_R_Si_Fi_Yh",
+                     "L111.Prod_EJ_R_F_Yh")
   if(command == driver.DECLARE_INPUTS) {
-    return(c("L1012.en_bal_EJ_R_Si_Fi_Yh_EUR"))
+    return(MODULE_INPUTS)
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L111.Prod_EJ_R_F_Yh_EUR"))
   } else if(command == driver.MAKE) {
@@ -30,13 +39,59 @@ module_gcameurope_L111.rsrc_fos_Prod <- function(command, ...) {
       available <- available_region_GCAM3 <- extractioncost <- technology <-  . <- NULL  # silence package check notes
 
     # Load required inputs
-    L1012.en_bal_EJ_R_Si_Fi_Yh_EUR <- get_data(all_data, "L1012.en_bal_EJ_R_Si_Fi_Yh_EUR", strip_attributes = TRUE)
+    get_data_list(all_data, MODULE_INPUTS)
 
-    # ------- HISTORICAL FOSSIL ENERGY PRODUCTION -------
-    # Determine production for each primary fuel by country
-    L111.Prod_EJ_R_F_Yh_EUR <- L1012.en_bal_EJ_R_Si_Fi_Yh_EUR %>%
-      filter(sector == "out_resources", fuel %in% energy.RSRC_FUELS, year %in% HISTORICAL_YEARS) %>%
-      mutate(technology = fuel)
+    # 1a. Fossil fuel production to be distributed to europe regions -------
+    # Keep non-Europe production the same and distribute europe production based on remainder
+    # NOTE: Regional production is derived for each fuel as TPES times regional share of production
+
+    # calculate production that must remain the same
+    L111.Prod_EJ_R_F_Yh_FIXED <- L111.Prod_EJ_R_F_Yh %>%
+      anti_join(L1012.en_bal_EJ_R_Si_Fi_Yh_EUR, by = "GCAM_region_ID") %>%
+      group_by(sector, fuel, year) %>%
+      summarise(value = sum(value)) %>%
+      ungroup %>%
+      mutate(fuel = case_when(
+        fuel == "coal" ~ "coal",
+        fuel == "crude oil" ~ "refined liquids",
+        fuel == "natural gas" ~ "gas"))
+
+    # remaining consumption that needs to be distributed
+    L111.TPES_EJ_F_Yh_REMAINDER <- L1012.en_bal_EJ_R_Si_Fi_Yh %>%
+      anti_join(L1012.en_bal_EJ_R_Si_Fi_Yh_EUR, by = c("GCAM_region_ID")) %>%
+      bind_rows(L1012.en_bal_EJ_R_Si_Fi_Yh_EUR) %>%
+      filter(sector == "TPES", fuel %in% energy.RSRC_FUELS, year %in% HISTORICAL_YEARS) %>%
+      group_by(sector, fuel, year) %>%
+      summarise(value = sum(value)) %>%
+      ungroup %>%
+      left_join_error_no_match(L111.Prod_EJ_R_F_Yh_FIXED, by = c("fuel", "year")) %>%
+      mutate(value = value.x - value.y) %>%
+      select(year, fuel, value)
+
+    # 1b. Regional shares of remaining production-------
+    # Determine regional shares of production for each primary fuel
+    L111.Prod_EJ_R_F_Yh_EUR_unadj <- L1012.en_bal_EJ_R_Si_Fi_Yh_EUR %>%
+      filter(sector == "out_resources", fuel %in% energy.RSRC_FUELS, year %in% HISTORICAL_YEARS)
+
+    L111.Prod_share_R_F_Yh_EUR <- L111.Prod_EJ_R_F_Yh_EUR_unadj %>%
+      group_by(sector, fuel, year) %>%
+      mutate(share = value / sum(value)) %>%
+      ungroup %>%
+      select(-value)
+
+    # 1c. Production by fuel -----------------------
+    # Multiply through to calculate production by fuel
+    L111.Prod_EJ_R_F_Yh_EUR <- L111.Prod_EJ_R_F_Yh_EUR_unadj %>%
+      select(-value) %>%
+      left_join_error_no_match(L111.TPES_EJ_F_Yh_REMAINDER, by = c("fuel", "year")) %>%
+      left_join_error_no_match(L111.Prod_share_R_F_Yh_EUR, by = c("GCAM_region_ID", "sector", "fuel", "year")) %>%
+      mutate(value = value * share) %>%
+      select(-share) %>%
+      mutate(fuel = case_when(
+        fuel == "coal" ~ "coal",
+        fuel == "refined liquids" ~ "crude oil",
+        fuel == "gas" ~ "natural gas"),
+        technology = fuel)
 
     # Produce outputs
     L111.Prod_EJ_R_F_Yh_EUR %>%
