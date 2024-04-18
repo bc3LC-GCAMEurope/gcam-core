e886633ea261c85e41081a8407da9ff10d430f76
russellhz <horowitz.russell@gmail.com>
2024-04-18 16:33:43 +0200

    EFW_input_coefs_EUR
    not sure if these are needed, but just in case

--- input/gcamdata/R/zgcameurope_xml_EFW_input_coefs.R
+++ input/gcamdata/R/zgcameurope_xml_EFW_input_coefs.R
@@ -1,36 +1,35 @@
 # Copyright 2019 Battelle Memorial Institute; see the LICENSE file.
 
-#' module_water_EFW_input_coefs_xml
+#' module_gcameurope_EFW_input_coefs_xml
 #'
-#' Construct XML data structure for \code{EFW_input_coefs.xml}.
+#' Construct XML data structure for \code{EFW_input_coefs_EUR.xml}.
 #'
 #' @param command API command to execute
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{EFW_input_coefs.xml}.
-module_water_EFW_input_coefs_xml <- function(command, ...) {
+#' the generated outputs: \code{EFW_input_coefs_EUR.xml}.
+module_gcameurope_EFW_input_coefs_xml <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
-    return(c("L270.TechCoef_EFW"))
+    return(c("L270.TechCoef_EFW_EUR"))
   } else if(command == driver.DECLARE_OUTPUTS) {
-    return(c(XML = "EFW_input_coefs.xml"))
+    return(c(XML = "EFW_input_coefs_EUR.xml"))
   } else if(command == driver.MAKE) {
 
     all_data <- list(...)[[1]]
 
     # Load required inputs
-    L270.TechCoef_EFW <- get_data(all_data, "L270.TechCoef_EFW")
+    L270.TechCoef_EFW_EUR <- get_data(all_data, "L270.TechCoef_EFW_EUR")
 
     # ===================================================
 
     # Produce outputs
-    create_xml("EFW_input_coefs.xml") %>%
-      add_xml_data(L270.TechCoef_EFW, "TechCoef") %>%
-      add_precursors("L270.TechCoef_EFW") %>%
-      remove_regions_xml(gcameurope.EUROSTAT_COUNTRIES) ->
-      EFW_input_coefs.xml
+    create_xml("EFW_input_coefs_EUR.xml") %>%
+      add_xml_data(L270.TechCoef_EFW_EUR, "TechCoef") %>%
+      add_precursors("L270.TechCoef_EFW_EUR") ->
+      EFW_input_coefs_EUR.xml
 
-    return_data(EFW_input_coefs.xml)
+    return_data(EFW_input_coefs_EUR.xml)
   } else {
     stop("Unknown command")
   }
