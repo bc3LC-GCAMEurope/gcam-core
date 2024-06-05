45adca8cc29413ec88b15dd270d2b2ace581c09c
russellhz <horowitz.russell@gmail.com>
2024-06-05 16:48:06 +0200

    first grid region chunks L1232,L1234

---
+++ input/gcamdata/R/zgcameurope_L1232.Elec_subregions.R
@@ -0,0 +1,57 @@
+# Copyright 2019 Battelle Memorial Institute; see the LICENSE file.
+
+#' module_gcameurope_L1232.Elec_subregions
+#'
+#' Aggregates European country electricity generation to electricity subregions.
+#'
+#' @param command API command to execute
+#' @param ... other optional parameters, depending on command
+#' @return Depends on \code{command}: either a vector of required inputs,
+#' a vector of output names, or (if \code{command} is "MAKE") all
+#' the generated outputs: \code{L1232.out_EJ_sR_elec}. The corresponding file in the
+#' original data system was \code{LB1232.Elec_subregions.R} (gcam-usa level1).
+#' @details Aggregates USA state electricity generation to electricity subregions.
+#' @importFrom assertthat assert_that
+#' @importFrom dplyr group_by left_join select summarise
+#' @author RLH September 2017
+module_gcameurope_L1232.Elec_subregions <- function(command, ...) {
+  if(command == driver.DECLARE_INPUTS) {
+    return(c(FILE = "gcam-europe/mappings/grid_regions",
+             FILE = "common/GCAM_region_names",
+             "L1231.out_EJ_R_elec_F_tech_Yh_EUR"))
+  } else if(command == driver.DECLARE_OUTPUTS) {
+    return(c("L1232.out_EJ_sR_elec_EUR"))
+  } else if(command == driver.MAKE) {
+
+    all_data <- list(...)[[1]]
+
+    # Silence package checks
+    state <- grid_region <- year <- value <- sector <- NULL
+
+    # Load required inputs
+    grid_regions <- get_data(all_data, "gcam-europe/mappings/grid_regions")
+    GCAM_region_names <- get_data(all_data, "common/GCAM_region_names")
+    L1231.out_EJ_R_elec_F_tech_Yh_EUR <- get_data(all_data, "L1231.out_EJ_R_elec_F_tech_Yh_EUR")
+
+    # ===================================================
+    # Aggregating states to electricity subregions
+    L1232.out_EJ_sR_elec_EUR <- L1231.out_EJ_R_elec_F_tech_Yh_EUR %>%
+      left_join_error_no_match(GCAM_region_names, by = "GCAM_region_ID") %>%
+      # Want to drop Belarus, Ukraine, Turkey, Moldova, Iceland
+      inner_join(grid_regions, by = "region") %>%
+      group_by(grid_region, sector, year) %>%
+      summarise(value = sum(value)) %>%
+      ungroup() %>%
+      # ===================================================
+    # Produce outputs
+    add_title("Electricity generation by grid region/fuel/technology") %>%
+      add_units("EJ") %>%
+      add_comments("L1231.out_EJ_R_elec_F_tech_Yh_EUR aggregated to grid region") %>%
+      add_precursors("gcam-europe/mappings/grid_regions",
+                     "L1231.out_EJ_R_elec_F_tech_Yh_EUR")
+
+    return_data(L1232.out_EJ_sR_elec_EUR)
+  } else {
+    stop("Unknown command")
+  }
+}
---
+++ input/gcamdata/R/zgcameurope_L1234.elec_gridregions.R
@@ -0,0 +1,79 @@
+# Copyright 2019 Battelle Memorial Institute; see the LICENSE file.
+
+#' module_gcameurope_L1234.elec_gridregions
+#'
+#' Calculate electricity fuel consumption and electricity generation by grid region.
+#'
+#' @param command API command to execute
+#' @param ... other optional parameters, depending on command
+#' @return Depends on \code{command}: either a vector of required inputs,
+#' a vector of output names, or (if \code{command} is "MAKE") all
+#' the listed outputs
+#' @details By grid region, calculates electricity fuel consumption and electricity generation.
+#' @importFrom assertthat assert_that
+#' @importFrom dplyr filter mutate select
+#' @author MTB August 2018
+module_gcameurope_L1234.elec_gridregions <- function(command, ...) {
+  MODULE_INPUTS <- c(FILE = "gcam-europe/mappings/grid_regions",
+                     FILE = "common/GCAM_region_names",
+                     "L123.out_EJ_R_elec_F_Yh_EUR",
+                     "L123.in_EJ_R_elec_F_Yh_EUR")
+  if(command == driver.DECLARE_INPUTS) {
+    return(MODULE_INPUTS)
+  } else if(command == driver.DECLARE_OUTPUTS) {
+    return(c("L1234.in_EJ_grid_elec_F_EUR",
+             "L1234.out_EJ_grid_elec_F_EUR"))
+  } else if(command == driver.MAKE) {
+
+    all_data <- list(...)[[1]]
+
+    # Silence package checks
+    grid_region <- sector <- fuel <- year <- fuel.input <- generation <- state <-
+      value <- NULL  # silence package check notes
+
+    # Load required inputs
+    get_data_list(all_data, MODULE_INPUTS)
+
+    # ===================================================
+    # Data Processing
+
+    # Take in country-level data on electric power sector fuel consumption, aggregate to grid regions
+    L1234.in_EJ_grid_elec_F_EUR <- L123.in_EJ_R_elec_F_Yh_EUR %>%
+      left_join_error_no_match(GCAM_region_names, by = "GCAM_region_ID") %>%
+      inner_join(grid_regions,  by ="region") %>%
+      group_by(grid_region, sector, fuel, year) %>%
+      summarise(fuel.input = sum(value)) %>%
+      ungroup()
+
+    # Take in state-level data on electricity generation by fuel, aggregate to grid regions
+    L1234.out_EJ_grid_elec_F_EUR <- L123.out_EJ_R_elec_F_Yh_EUR %>%
+      left_join_error_no_match(GCAM_region_names, by = "GCAM_region_ID") %>%
+      inner_join(grid_regions,  by ="region") %>%
+      group_by(grid_region, sector, fuel, year) %>%
+      summarise(generation = sum(value)) %>%
+      ungroup()
+
+    # ===================================================
+    # Produce outputs
+
+    L1234.in_EJ_grid_elec_F_EUR %>%
+      add_title("Fuel input into electricity by fuel and grid region") %>%
+      add_units("EJ") %>%
+      add_comments("Fuel input values from L123.in_EJ_state_elec_F aggregated to grid region level") %>%
+      add_precursors("L123.in_EJ_R_elec_F_Yh_EUR",
+                     "gcam-europe/mappings/grid_regions") ->
+      L1234.in_EJ_grid_elec_F_EUR
+
+    L1234.out_EJ_grid_elec_F_EUR %>%
+      add_title("Electricity generation by fuel and grid region") %>%
+      add_units("EJ") %>%
+      add_comments("Electricity generation (output) values from L123.out_EJ_state_elec_F aggregated to grid region level") %>%
+      add_precursors("L123.out_EJ_R_elec_F_Yh_EUR",
+                     "gcam-europe/mappings/grid_regions") ->
+      L1234.out_EJ_grid_elec_F_EUR
+
+    return_data(L1234.in_EJ_grid_elec_F_EUR, L1234.out_EJ_grid_elec_F_EUR)
+  } else {
+    stop("Unknown command")
+  }
+}
