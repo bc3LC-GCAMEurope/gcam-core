be9acd89348213fa9c03d1c222176f9d4e8c6312
russellhz <horowitz.russell@gmail.com>
2024-11-20 16:20:41 +0100

    ammonia doble armington

--- input/gcamdata/R/zgcameurope_xml_en_Fert.R
+++ input/gcamdata/R/zgcameurope_xml_en_Fert.R
@@ -11,19 +11,30 @@
 #' the generated outputs: \code{en_Fert_EUR.xml}. The corresponding file in the
 #' original data system was \code{batch_en_Fert_xml.R} (energy XML).
 module_gcameurope_en_Fert_xml <- function(command, ...) {
+  MODULE_INPUTS <- c("L2322.Supplysector_Fert_EUR",
+                     "L2322.SectorUseTrialMarket_tra_EUR",
+                     "L2322.FinalEnergyKeyword_Fert_EUR",
+                     "L2322.SubsectorLogit_Fert_EUR",
+                     "L2322.SubsectorShrwtFllt_Fert_EUR",
+                     "L2322.SubsectorInterp_Fert_EUR",
+                     "L2322.StubTech_Fert_EUR",
+                     "L2322.TechShrwt_TradedFert_EUR",
+                     "L2322.TechCoef_TradedFert_EUR",
+                     "L2322.StubTechMarket_FertImports_EUR",
+                     "L2322.StubTechProd_FertProd_EUR",
+                     "L2322.StubTechCoef_Fert_EUR",
+                     "L2322.Production_FertExport_EUR",
+                     "L2322.StubTechProd_FertImport_EUR",
+                     "L2322.StubTechProd_FertDomCons_EUR",
+                     "L2322.StubTechProd_NtoAg_EUR",
+                     "L2322.GlobalTechCost_Fert",
+                     "L2322.GlobalTechCapture_Fert",
+                     "L2322.GlobalTechSCurve_Fert",
+                     "L2322.GlobalTechProfitShutdown_Fert",
+                     "L2322.GlobalTechCoef_Fert",
+                     "L2322.GlobalTechShrwt_Fert")
   if(command == driver.DECLARE_INPUTS) {
-    return(c("L2322.Supplysector_Fert_EUR",
-             "L2322.FinalEnergyKeyword_Fert_EUR",
-             "L2322.SubsectorLogit_Fert_EUR",
-             "L2322.SubsectorShrwtFllt_Fert_EUR",
-             "L2322.SubsectorInterp_Fert_EUR",
-             "L2322.StubTech_Fert_EUR",
-             "L2322.StubTechMarket_FertImports_EUR",
-             "L2322.StubTechProd_FertProd_EUR",
-             "L2322.StubTechCoef_Fert_EUR",
-             "L2322.StubTechProd_FertImport_EUR",
-             "L2322.StubTechProd_FertDomCons_EUR",
-             "L2322.StubTechProd_NtoAg_EUR"))
+    return(MODULE_INPUTS)
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c(XML = "en_Fert_EUR.xml"))
   } else if(command == driver.MAKE) {
@@ -31,47 +42,34 @@ module_gcameurope_en_Fert_xml <- function(command, ...) {
     all_data <- list(...)[[1]]
 
     # Load required inputs
-    L2322.Supplysector_Fert_EUR <- get_data(all_data, "L2322.Supplysector_Fert_EUR")
-    L2322.FinalEnergyKeyword_Fert_EUR <- get_data(all_data, "L2322.FinalEnergyKeyword_Fert_EUR")
-    L2322.SubsectorLogit_Fert_EUR <- get_data(all_data, "L2322.SubsectorLogit_Fert_EUR")
-    L2322.SubsectorShrwtFllt_Fert_EUR <- get_data(all_data, "L2322.SubsectorShrwtFllt_Fert_EUR")
-    L2322.SubsectorInterp_Fert_EUR <- get_data(all_data, "L2322.SubsectorInterp_Fert_EUR")
-    L2322.StubTech_Fert_EUR <- get_data(all_data, "L2322.StubTech_Fert_EUR")
-    L2322.StubTechMarket_FertImports_EUR <- get_data(all_data, "L2322.StubTechMarket_FertImports_EUR")
-    L2322.StubTechProd_FertProd_EUR <- get_data(all_data, "L2322.StubTechProd_FertProd_EUR")
-    L2322.StubTechCoef_Fert_EUR <- get_data(all_data, "L2322.StubTechCoef_Fert_EUR")
-    L2322.StubTechProd_FertImport_EUR <- get_data(all_data, "L2322.StubTechProd_FertImport_EUR")
-    L2322.StubTechProd_FertDomCons_EUR <- get_data(all_data, "L2322.StubTechProd_FertDomCons_EUR")
-    L2322.StubTechProd_NtoAg_EUR <- get_data(all_data, "L2322.StubTechProd_NtoAg_EUR")
-
+    get_data_list(all_data, MODULE_INPUTS)
     # ===================================================
 
     # Produce outputs
     create_xml("en_Fert_EUR.xml") %>%
       add_logit_tables_xml(L2322.Supplysector_Fert_EUR, "Supplysector") %>%
+      add_xml_data(L2322.SectorUseTrialMarket_tra_EUR, "SectorUseTrialMarket") %>%
       add_xml_data(L2322.FinalEnergyKeyword_Fert_EUR, "FinalEnergyKeyword") %>%
       add_logit_tables_xml(L2322.SubsectorLogit_Fert_EUR, "SubsectorLogit") %>%
       add_xml_data(L2322.SubsectorShrwtFllt_Fert_EUR, "SubsectorShrwtFllt") %>%
       add_xml_data(L2322.SubsectorInterp_Fert_EUR, "SubsectorInterp") %>%
       add_xml_data(L2322.StubTech_Fert_EUR, "StubTech") %>%
+      add_xml_data(L2322.TechShrwt_TradedFert_EUR, "TechShrwt") %>%
+      add_xml_data(L2322.TechCoef_TradedFert_EUR, "TechCoef") %>%
       add_xml_data(L2322.StubTechMarket_FertImports_EUR, "StubTechMarket") %>%
       add_xml_data(L2322.StubTechProd_FertProd_EUR, "StubTechProd") %>%
       add_xml_data(L2322.StubTechCoef_Fert_EUR, "StubTechCoef") %>%
+      add_xml_data(L2322.Production_FertExport_EUR, "Production") %>%
       add_xml_data(L2322.StubTechProd_FertImport_EUR, "StubTechProd") %>%
       add_xml_data(L2322.StubTechProd_FertDomCons_EUR, "StubTechProd") %>%
       add_xml_data(L2322.StubTechProd_NtoAg_EUR, "StubTechProd") %>%
-      add_precursors("L2322.Supplysector_Fert_EUR",
-                     "L2322.FinalEnergyKeyword_Fert_EUR",
-                     "L2322.SubsectorLogit_Fert_EUR",
-                     "L2322.SubsectorShrwtFllt_Fert_EUR",
-                     "L2322.SubsectorInterp_Fert_EUR",
-                     "L2322.StubTech_Fert_EUR",
-                     "L2322.StubTechMarket_FertImports_EUR",
-                     "L2322.StubTechProd_FertProd_EUR",
-                     "L2322.StubTechCoef_Fert_EUR",
-                     "L2322.StubTechProd_FertImport_EUR",
-                     "L2322.StubTechProd_FertDomCons_EUR",
-                     "L2322.StubTechProd_NtoAg_EUR") ->
+      add_xml_data(L2322.GlobalTechCost_Fert, "GlobalTechCost") %>%
+      add_xml_data(L2322.GlobalTechCapture_Fert, "GlobalTechCapture") %>%
+      add_xml_data(L2322.GlobalTechSCurve_Fert, "GlobalTechSCurve") %>%
+      add_xml_data(L2322.GlobalTechProfitShutdown_Fert, "GlobalTechProfitShutdown") %>%
+      add_xml_data(L2322.GlobalTechCoef_Fert, "GlobalTechCoef") %>%
+      add_xml_data(L2322.GlobalTechShrwt_Fert, "GlobalTechShrwt") %>%
+      add_precursors(MODULE_INPUTS) ->
       en_Fert_EUR.xml
 
     return_data(en_Fert_EUR.xml)
