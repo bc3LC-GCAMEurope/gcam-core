24d53a81dae49aa779250d7ca94906eff18fabc9
Jon Sampedro <jon.sampedro@bc3.net>
2025-10-15 10:58:56 +0200

    Delete UK and UKraine from EUR-electricity

--- input/gcamdata/R/zwater_xml_electricity_water.R
+++ input/gcamdata/R/zwater_xml_electricity_water.R
@@ -271,7 +271,7 @@ module_water_electricity_water_xml <- function(command, ...) {
                      "L2233.GlobalTechCapital_elec_cool",
                      "L2233.GlobalIntTechCapital_elec_cool",
                      "L223.GlobalTechCapFac_elec") %>%
-      remove_regions_xml(c(gcameurope.EUROSTAT_COUNTRIES, "Switzerland")) ->
+      remove_regions_xml(c(gcameurope.EUROSTAT_COUNTRIES, "Switzerland", gcameurope.EUROSTAT_ADJCOUNTRIES)) ->
       electricity_water.xml
 
     return_data(electricity_water.xml)
