e0c00ab4c581786fd564872674b1e9b1954aba19
klau506 <klaudia.krb@gmail.com>
2026-03-13 14:30:23 +0100

    simplify NationalAccounts proxies

--- input/gcamdata/R/zsocio_L100.NationalAccounts.R
+++ input/gcamdata/R/zsocio_L100.NationalAccounts.R
@@ -22,7 +22,7 @@ module_socio_L100.NationalAccounts <- function(command, ...) {
       FILE = "socioeconomics/NationalAccounts/pwt1001",
       FILE = "socioeconomics/NationalAccounts/NationalAccounts_variable_mapping",
       FILE = "socioeconomics/NationalAccounts/GMD_2025_03",
-      # FILE = "gcam-europe/A100.proxies",
+      FILE = "gcam-europe/A100.proxies",
       "L100.GTAP_capital_stock")
 
   MODULE_OUTPUTS <-
@@ -50,31 +50,31 @@ module_socio_L100.NationalAccounts <- function(command, ...) {
     get_data_list(all_data, MODULE_INPUTS, strip_attributes = TRUE)
 
     # 0. Help function to find proxy-region for regions that ----
-    ## do not satisfy the required socioeconomic criteria
-    # proxy_socioecon_eu <- function(data, sign, threshold) {
-    #   operator <- switch(as.character(sign)[[1]],
-    #                      "lt" = `<`,
-    #                      "gt" = `>`,
-    #                      "le" = `<=`,
-    #                      "ge" = `>=`,
-    #                      "eq" = `==`)
-    #   lookup_values <- setNames(data$value, as.character(data$GCAM_region_ID))
-    #
-    #   # save the initial column names
-    #   ini_colnames <- colnames(data)
-    #
-    #   tmp <- data %>%
-    #     # use the value of the proxy-region if the threshold is not satisfied
-    #     left_join(A100.proxies, by = 'GCAM_region_ID') %>%
-    #     mutate(value = if_else(!is.na(proxy_GCAM_region_ID) & operator(value, threshold),
-    #                            lookup_values[as.character(proxy_GCAM_region_ID)],
-    #                            value)) %>%
-    #     # select only the original columns
-    #     select(ini_colnames)
-    #
-    #   return(tmp)
-    #
-    # }
+    # do not satisfy the required socioeconomic criteria
+    proxy_socioecon_eu <- function(data, sign, threshold) {
+      operator <- switch(as.character(sign)[[1]],
+                         "lt" = `<`,
+                         "gt" = `>`,
+                         "le" = `<=`,
+                         "ge" = `>=`,
+                         "eq" = `==`)
+      lookup_values <- setNames(data$value, as.character(data$GCAM_region_ID))
+
+      # save the initial column names
+      ini_colnames <- colnames(data)
+
+      tmp <- data %>%
+        # use the value of the proxy-region if the threshold is not satisfied
+        left_join(A100.proxies, by = 'GCAM_region_ID') %>%
+        mutate(value = if_else(!is.na(proxy_GCAM_region_ID) & operator(value, threshold),
+                               lookup_values[as.character(proxy_GCAM_region_ID)],
+                               value)) %>%
+        # select only the original columns
+        select(ini_colnames)
+
+      return(tmp)
+
+    }
 
 
     # 0. Help function to pull data from different sources ----
@@ -205,10 +205,9 @@ module_socio_L100.NationalAccounts <- function(command, ...) {
     # adding assertions to ensure the values are bounded in [0.25, 0.8] based on recent observations. Similar assertions are used later
     # to flag any potential issues in future data updates.
     assertthat::assert_that(L100.National_Accounts_Employment_Share_POP_R_Yh %>% filter(year == Socioeconomic.PWT.LastYear) %>%
-                              #proxy_socioecon_eu(., 'lt', 0.25) %>%
-                              pull(value) %>% min > 0.20, msg = "check min value in data")
+                              proxy_socioecon_eu(., 'lt', 0.25) %>%          # NOTE 13/03/2026: Bosnia&Hzg has 0.209 employment rate in 2019. The value of Macedonia is used as proxy.
+                              pull(value) %>% min > 0.25, msg = "check min value in data")
     assertthat::assert_that(L100.National_Accounts_Employment_Share_POP_R_Yh %>% filter(year == Socioeconomic.PWT.LastYear) %>%
-                              #proxy_socioecon_eu(., 'gt', 0.8) %>%
                               pull(value) %>% max < 0.8, msg = "check max value in data")
 
     L100.National_Accounts_Employment_Share_POP_R_Yh %>%
@@ -309,10 +308,9 @@ module_socio_L100.NationalAccounts <- function(command, ...) {
 
     # adding assertions to ensure the values are bounded in [0.8, 5] based on recent observations.
     assertthat::assert_that(Capital_GDP_Ratio_R_Yh %>%
-                              #proxy_socioecon_eu(., 'lt', 0.8) %>%
-                              pull(value) %>% min > 0.6, msg = "check min value in data")
+                              proxy_socioecon_eu(., 'lt', 0.8) %>%          # NOTE 13/03/2026: use some proxies for Bulgaria from 1971-1976, which has values ranging from 0.6-0.78
+                              pull(value) %>% min > 0.8, msg = "check min value in data")
     assertthat::assert_that(Capital_GDP_Ratio_R_Yh %>%
-                              #proxy_socioecon_eu(., 'gt', 5) %>%
                               pull(value) %>% max > 5, msg = "check max value in data")
     assertthat::assert_that(Capital_GDP_Ratio_R_Yh %>%
                               anti_join(GCAM_region_names, ., by = "GCAM_region_ID") %>% nrow == 0,
@@ -389,10 +387,8 @@ module_socio_L100.NationalAccounts <- function(command, ...) {
                             msg = "not all GCAM region available in data")
     # adding assertions to ensure the values are bounded in [0.2, 0.8] based on recent observations.
     assertthat::assert_that(Labor_Compensation_Share_R_Yh %>%
-                              #proxy_socioecon_eu(., 'lt', 0.2) %>%
                               pull(value) %>% min > 0.2, msg = "check min value in data")
     assertthat::assert_that(Labor_Compensation_Share_R_Yh %>%
-                              #proxy_socioecon_eu(., 'gt', 0.8) %>%
                               pull(value) %>% max > 0.8, msg = "check max value in data")
 
 
@@ -533,10 +529,9 @@ module_socio_L100.NationalAccounts <- function(command, ...) {
       L100.National_Accounts_En_capital_inv_share_R_Yh
 
     assertthat::assert_that(L100.National_Accounts_En_capital_inv_share_R_Yh %>%
-                              #proxy_socioecon_eu(., 'lt', 0.04) %>%
-                              pull(value) %>% min > 0.01, msg = "check min value in data")
+                              proxy_socioecon_eu(., 'lt', 0.02) %>%           # NOTE 13/03/2026: use proxies for some 0.018 values
+                              pull(value) %>% min > 0.02, msg = "check min value in data")
     assertthat::assert_that(L100.National_Accounts_En_capital_inv_share_R_Yh %>%
-                              #proxy_socioecon_eu(., 'gt', 0.5) %>%
                               pull(value) %>% max < 0.5, msg = "check max value in data")
     assertthat::assert_that(L100.National_Accounts_En_capital_inv_share_R_Yh %>%
                               anti_join(GCAM_region_names, ., by = "GCAM_region_ID") %>% nrow == 0,
